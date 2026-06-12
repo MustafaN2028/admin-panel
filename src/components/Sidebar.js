9f2c429cbdb005fac90f4b5bfee07caf765790c1
MustafaN2028 <mustafa.md@federalsoftsystems.com>
2026-06-12 18:00:42 +0530

    component memoization

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -18,15 +18,26 @@ function Sidebar({ isOpen }) {
         <ShieldLock size={22} className="text-primary" />
       </div>
       <Nav className="flex-column gap-2 flex-grow-1">
-        <Link href="/">
+        <Link
+          href="/"
+          className={`text-white p-2 text-decoration-none ${!isOpen ? "text-center" : ""}`}
+        >
           <Speedometer2 size={18} />
-          {isOpen && <span>Dashboard</span>}
+          {isOpen && <span className="ms-1">Dashboard</span>}
         </Link>
-        <Link href="user">
-          <People size={18} /> {isOpen && <span>User Directory</span>}
+        <Link
+          href="user"
+          className={`text-white p-2 text-decoration-none ${!isOpen ? "text-center" : ""}`}
+        >
+          <People size={18} />{" "}
+          {isOpen && <span className="ms-1">User Directory</span>}
         </Link>
-        <Link href="subscription">
-          <GraphUp size={18} /> {isOpen && <span>Subscription</span>}
+        <Link
+          href="subscription"
+          className={`text-white p-2 text-decoration-none ${!isOpen ? "text-center" : ""}`}
+        >
+          <GraphUp size={18} />{" "}
+          {isOpen && <span className="ms-1">Subscription</span>}
         </Link>
       </Nav>
     </div>
